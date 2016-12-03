43b3dee1e51aaa1e9253c440f8ad6392330fffc2
Sarah Walters <swalters4925@gmail.com>
2016-12-02 23:14:11 -0500

    FIX: pulling magic numbers out of ship/sail into params

--- project/alpha/js/contrib.js
+++ project/alpha/js/contrib.js
@@ -1,27 +1,89 @@
 var SWALTER2_PROJECT = (function() {
 	var clothGeometry;
 
-	// set up cloth simulator
-	var pins = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-				 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120];
-	function windForceFunction(time) {
-		var windStrength = Math.cos( time / 7000 ) * 20 + 40;
-		var windForce = new THREE.Vector3(
-			Math.abs( Math.sin( time / 2000 ) ),
-			Math.abs( Math.cos( time / 3000 ) ),
-			Math.abs( Math.sin( time / 1000 ) )
-		);
-		return windForce.normalize()
-			            .multiplyScalar( windStrength );
-	}
-	function constraintFunction(particle) {
-		// keeps the sail in front of the plane the poles lie in
-		pos = particle.position;
-		if ( pos.z < 5 / 2 ) {
-			pos.z = 5 / 2;
+	// set up scene parameters
+	var params = (function() {
+		var loader = new THREE.TextureLoader();
+
+		// pole parameters
+		var poleSize = 5;
+		var verticalPoleHeight = 375;
+		var horizontalPoleLength = 255;
+		var poleMat = new THREE.MeshPhongMaterial({
+			color: 0xffffff, specular: 0x111111, shininess: 100
+		});
+
+		// sail cloth parameters
+		var clothTexture = loader.load( 'textures/fabric.jpg' );
+		clothTexture.wrapS = clothTexture.wrapT = THREE.RepeatWrapping;
+		clothTexture.anisotropy = 16;
+		var clothMaterial = new THREE.MeshPhongMaterial({
+			specular: 0x030303,
+			map: clothTexture,
+			side: THREE.DoubleSide,
+			alphaTest: 0.5
+		});
+
+		var pins = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+				    110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120];
+
+		function constraintFunction(particle) {
+			// keeps the sail in front of the plane the poles lie in
+			pos = particle.position;
+			if (pos.z < poleSize / 2) {
+				pos.z = poleSize / 2;
+			}
 		}
-	}
-	var CLOTH = new ClothSimulation(pins, windForceFunction, constraintFunction);
+
+		function windForceFunction(time) {
+			var windStrength = Math.cos( time / 7000 ) * 20 + 40;
+			var windForce = new THREE.Vector3(
+				Math.abs( Math.sin( time / 2000 ) ),
+				Math.abs( Math.cos( time / 3000 ) ),
+				Math.abs( Math.sin( time / 1000 ) )
+			);
+			return windForce.normalize().multiplyScalar(windStrength);
+		}
+
+		// ship parameters
+		var shipControlPoints = [
+			[ [-1/2,2/3,0], [-1/6,-1/3,0], [1/6,-1/3,0], [1/2,2/3,0] ],
+            [ [-1/2,2/3,0], [-1/6,0,1], [1/6,0,1], [1/2,2/3,0] ],
+            [ [-1/2,2/3,0], [-1/6,1/3,1], [1/6,1/3,1], [1/2,2/3,0] ],
+            [ [-1/2,2/3,0], [-1/6,2/3,1], [1/6,2/3,1], [1/2,2/3,0] ]
+        ];
+
+        var shipMaterial = new THREE.MeshNormalMaterial({
+        	side: THREE.DoubleSide
+        });
+
+        var shipDims = {length: 600, height: 90, width: 75};
+
+		return {
+			// pole parameters
+			poleSize: poleSize,
+			verticalPoleHeight: verticalPoleHeight,
+			horizontalPoleLength: horizontalPoleLength,
+			poleMat: poleMat,
+
+			// sail cloth parameters
+			pins: pins,
+			clothTexture: clothTexture,
+			clothMaterial: clothMaterial,
+			constraintFunction: constraintFunction,
+			windForceFunction: windForceFunction,
+
+			// ship parameters
+			shipControlPoints: shipControlPoints,
+			shipMaterial: shipMaterial,
+			shipDims: shipDims
+		}
+	})();
+
+	// set up cloth simulator
+	var CLOTH = new ClothSimulation(params.pins,
+									params.windForceFunction,
+									params.constraintFunction);
 
 	// Origin is at center of base of vertical supporting pole
 	// X axis runs parallel to horizontal supporting poles
@@ -29,30 +91,17 @@ var SWALTER2_PROJECT = (function() {
 	function Sail() {
 		var result = new THREE.Object3D();
 
-		// cloth material
-		var loader = new THREE.TextureLoader();
-		var clothTexture = loader.load( 'textures/fabric.jpg' );
-		clothTexture.wrapS = clothTexture.wrapT = THREE.RepeatWrapping;
-		clothTexture.anisotropy = 16;
-
-		var clothMaterial = new THREE.MeshPhongMaterial( {
-			specular: 0x030303,
-			map: clothTexture,
-			side: THREE.DoubleSide,
-			alphaTest: 0.5
-		} );
-
 		// cloth geometry
-		clothGeometry = new THREE.ParametricGeometry( CLOTH.clothFunction, CLOTH.w, CLOTH.h );
+		clothGeometry = new THREE.ParametricGeometry(CLOTH.clothFunction, CLOTH.w, CLOTH.h);
 		clothGeometry.dynamic = true;
 
-		var uniforms = { texture:  { value: clothTexture } };
+		var uniforms = { texture:  { value: params.clothTexture } };
 		var vertexShader = document.getElementById( 'vertexShaderDepth' ).textContent;
 		var fragmentShader = document.getElementById( 'fragmentShaderDepth' ).textContent;
 
 		// cloth mesh
-		object = new THREE.Mesh( clothGeometry, clothMaterial );
-		object.position.set(0, 0, 5 / 2);
+		object = new THREE.Mesh(clothGeometry, params.clothMaterial);
+		object.position.set(0, 0, params.poleSize / 2);
 		object.castShadow = true;
 		result.add( object );
 
@@ -64,51 +113,52 @@ var SWALTER2_PROJECT = (function() {
 		} );
 
 		// poles
-		var poleGeo = new THREE.BoxGeometry( 5, 375, 5 );
-		var crossbarGeo = new THREE.BoxGeometry( 255, 5, 5 );
-		var capGeo = new THREE.BoxGeometry( 10, 10, 10 );
-		var poleMat = new THREE.MeshPhongMaterial( { color: 0xffffff, specular: 0x111111, shininess: 100 } );
+		var verticalPoleGeo = new THREE.BoxGeometry(params.poleSize, params.verticalPoleHeight, params.poleSize);
+		var crossbarGeo = new THREE.BoxGeometry(params.horizontalPoleLength, params.poleSize, params.poleSize);
+		var capGeo = new THREE.BoxGeometry(params.poleSize * 2, params.poleSize * 2, params.poleSize * 2);
 
-		var mesh = new THREE.Mesh( poleGeo, poleMat );
-		mesh.position.set(0, 375 / 2, 0);
-		mesh.receiveShadow = true;
-		mesh.castShadow = true;
-		result.add( mesh );
+		var verticalPole = new THREE.Mesh(verticalPoleGeo, params.poleMat);
+		verticalPole.position.set(0, params.verticalPoleHeight / 2, 0);
+		verticalPole.receiveShadow = true;
+		verticalPole.castShadow = true;
+		result.add(verticalPole);
 
-		var mesh = new THREE.Mesh( crossbarGeo, poleMat );
-		mesh.position.set(0, 750 / 2, 0);
-		mesh.receiveShadow = true;
-		mesh.castShadow = true;
-		result.add( mesh );
+		var topCrossbar = new THREE.Mesh(crossbarGeo, params.poleMat);
+		topCrossbar.position.set(0, params.verticalPoleHeight, 0);
+		topCrossbar.receiveShadow = true;
+		topCrossbar.castShadow = true;
+		result.add(topCrossbar);
 
-		var mesh = new THREE.Mesh( crossbarGeo, poleMat );
-		mesh.position.set(0, 750 / 6, 0);
-		mesh.receiveShadow = true;
-		mesh.castShadow = true;
-		result.add( mesh );
+		var bottomCrossbar = new THREE.Mesh(crossbarGeo, params.poleMat);
+		bottomCrossbar.position.set(0, params.verticalPoleHeight / 3, 0);
+		bottomCrossbar.receiveShadow = true;
+		bottomCrossbar.castShadow = true;
+		result.add(bottomCrossbar);
 
-		var mesh = new THREE.Mesh( capGeo, poleMat );
-		mesh.position.set(0, 0, 0);
-		mesh.receiveShadow = true;
-		mesh.castShadow = true;
-		result.add( mesh );
+		var cap = new THREE.Mesh(capGeo, params.poleMat);
+		cap.position.set(0, 0, 0);
+		cap.receiveShadow = true;
+		cap.castShadow = true;
+		result.add(cap);
 
 		return result;
 	}
 
 	function Ship() {
-		var ballGeo = new THREE.SphereGeometry(5);
+		var result = new THREE.Object3D();
 
-		var loader = new THREE.TextureLoader();
-		var clothTexture = loader.load( 'textures/fabric.jpg' );
-		var ballMat = new THREE.MeshPhongMaterial( {
-			specular: 0x030303,
-			map: clothTexture,
-			side: THREE.DoubleSide,
-			alphaTest: 0.5
-		} );
+        var halfShipGeo = new THREE.BezierSurfaceGeometry(params.shipControlPoints, 10, 10);
 
-		return new THREE.Mesh(ballGeo, ballMat);
+        var half1 = new THREE.Mesh(halfShipGeo, params.shipMaterial);
+        result.add(half1);
+
+        var half2 = new THREE.Mesh(halfShipGeo, params.shipMaterial);
+        half2.rotation.set(0, Math.PI, 0);
+        result.add(half2);
+
+        result.scale.set(params.shipDims.length, params.shipDims.height, params.shipDims.width);
+        result.rotation.set(0, Math.PI / 2, 0);
+		return result;
 	}
 
 	function render() {
